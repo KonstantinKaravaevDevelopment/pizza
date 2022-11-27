3b03ee1cd751e082e6a3324f75d3188f8afc46ab
KonstantinKaravaevDevelopment <91592318+KonstantinKaravaevDevelopment@users.noreply.github.com>
2022-11-27 13:38:08 +0100

    Delete technopizzeria directory

--- technopizzeria/pizzeria/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Dania_Pizzeria
-
-# Register your models here.
-admin.site.register(Dania_Pizzeria)
--- technopizzeria/pizzeria/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PizzeriaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pizzeria'
